3c936f7eca457c7ec462f66352adaf04cd0ccdbd
Elguja Modebadze <gujiko.modebadze@gmail.com>
2025-08-17 20:35:17 +0400

    setup test routes. wrote html files.

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const { createServer } = require('http');
-const PORT = 5000;
+const PORT = 8080;
 
 const server = createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>server working')
+    if (req.url === "/") {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>index</h1>')
+    } else if (req.url === "/about") {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>about</h1>');
+    } else if (req.url === "/contact-me") {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>contacts</h1>');
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('<h1>page does not exist</h1>')
+    }
 })
 
 server.listen(PORT, console.log('Server running at ' + PORT))
